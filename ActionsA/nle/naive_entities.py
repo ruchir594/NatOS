b2054ebebc99648a93c722935e17c50c4858c5ed
Ruchir Patel <ruchir594@gmail.com>
2017-04-06 12:05:41 +0530

    fcheck 1

--- ActionsA/nle/naive_entities.py
+++ ActionsA/nle/naive_entities.py
@@ -48,6 +48,8 @@ class stamps:
         self.reminder = [] #
         # reminder on or off, for snooze
 
+        self.location = []
+
         self.reminder_text = []
         # what would be reminded about, this text should be generated while making
         # above "True"
@@ -62,6 +64,25 @@ class stamps:
         # words that may be important and could be used by KGB but don't seem to fit anywhere
 
         self.checked = None
+
+        self.d1 = ['i', 'live', 'in', 'please', 'hi', 'give', 'find', 'who', 'what', 'my', 'hungry', 'near', 'me', 'thank', 'you', \
+                'want', 'to', 'eat', 'like','liked', 'I', 'can', 'you', 'suggest', 'of', 'is', 'are', 'near', 'there', 'some', \
+                'little', 'now', 'wanna', 'want', 'at', 'on', 'in', 'near', 'area', 'next', 'and', 'how', 'about', 'or', \
+                'the', 'a', 'an', 'about', 'for', 'with', 'should', 'could', 'would', 'out','time','person','year','way','day',\
+                'thing','man','world','life','hand','part','child','eye','woman','place','work','week', 'doing',\
+                'case','point','government','company','number','group','problem','fact','be','have','do','say',\
+                'get','make','go','know','take','see','come','think','look','want','give','use','find','tell', 'telling',\
+                'ask','work','seem','feel','try','leave','call','good','new','first','last','long','great','little','own','other',\
+                'old','right','big','high','different','small','large','next','early','young','important','few',\
+                'public','bad','same','able','to','of','in','for','on','with','at','by','from','up','about','into',\
+                'over','after','beneath','under','above','the','and','a','that','I','it','not','he','as','you', \
+                'this','but','his','they','her','she','or','an','will','my','one','all','would','there','their', 'talk', \
+                'talking', 'love', 'loved', 'hello', 'help', 'helping', 'helped', 'pleasure', 'bye', 'goodbye', 'care', 'later', \
+                'no','nothing', 'thanks', 'welcome', 'something', 'hey', 'am', 'month','year','week','day','hour','minute','min','second', \
+                'months','years','weeks','days','hours','minutes','mins','seconds','time', 'today', 'tomorrow', 'am', 'pm',\
+                'january', 'febuary', 'marth', 'april', 'may', 'june', 'july','august', 'september', 'october', 'november', 'december', \
+                'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday','km', 'kilometer', 'kilometers', 'meter', 'm',\
+                'cm', 'kms', 'miles', 'yards', 'feet', 'feets','evening', 'morning', 'afternoon', 'noon', 'night']
 
     def view(self):
         print ("self.protagonist```````", self.protagonist + "]")
@@ -106,25 +127,7 @@ class stamps:
          "self.checked```````````": self.checked
          })
 
-    d1 = ['i', 'live', 'in', 'please', 'hi', 'give', 'find', 'who', 'what', 'my', 'hungry', 'near', 'me', 'thank', 'you', \
-            'want', 'to', 'eat', 'like','liked', 'I', 'can', 'you', 'suggest', 'of', 'is', 'are', 'near', 'there', 'some', \
-            'little', 'now', 'wanna', 'want', 'at', 'on', 'in', 'near', 'area', 'next', 'and', 'how', 'about', 'or', \
-            'the', 'a', 'an', 'about', 'for', 'with', 'should', 'could', 'would', 'out','time','person','year','way','day',\
-            'thing','man','world','life','hand','part','child','eye','woman','place','work','week', 'doing',\
-            'case','point','government','company','number','group','problem','fact','be','have','do','say',\
-            'get','make','go','know','take','see','come','think','look','want','give','use','find','tell', 'telling',\
-            'ask','work','seem','feel','try','leave','call','good','new','first','last','long','great','little','own','other',\
-            'old','right','big','high','different','small','large','next','early','young','important','few',\
-            'public','bad','same','able','to','of','in','for','on','with','at','by','from','up','about','into',\
-            'over','after','beneath','under','above','the','and','a','that','I','it','not','he','as','you', \
-            'this','but','his','they','her','she','or','an','will','my','one','all','would','there','their', 'talk', \
-            'talking', 'love', 'loved', 'hello', 'help', 'helping', 'helped', 'pleasure', 'bye', 'goodbye', 'care', 'later', \
-            'no','nothing', 'thanks', 'welcome', 'something', 'hey', 'am', 'month','year','week','day','hour','minute','min','second', \
-            'months','years','weeks','days','hours','minutes','mins','seconds','time', 'today', 'tomorrow', 'am', 'pm',\
-            'january', 'febuary', 'marth', 'april', 'may', 'june', 'july','august', 'september', 'october', 'november', 'december', \
-            'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday','km', 'kilometer', 'kilometers', 'meter', 'm',\
-            'cm', 'kms', 'miles', 'yards', 'feet', 'feets','evening', 'morning', 'afternoon', 'noon', 'night']
-            
+
     def fill_init(self):
 
         duration_text = ['month','year','week','day','hour','minute','min','second', \
@@ -140,14 +143,14 @@ class stamps:
         distance_text = ['km', 'kilometer', 'kilometers', 'meter', 'm', 'cm', 'kms', 'miles', 'yards', 'feet', 'feets']
         amount_of_money = ['usd', 'dollar', 'dollars', 'euro', 'pound', 'pounds', 'euros', 'rupee', 'rupees', 'rs']
 
-        b = self.getWords(aliner)
+        b = getWords(self.aliner)
 
         self.checked = [False] * len(b)
 
         i=0
         for i in range(len(b)):
             if b[i] in day_text:
-                self.day_text.append(b[i])
+                self.day.append(b[i])
                 self.checked[i] = True
 
 
@@ -161,13 +164,14 @@ class stamps:
 
 
     def completia(self):
+        a='one'
 
 
     def just_location_plus(self):
         c = getWords_special_location(self.aliner)
         a = ''
         for c_cmall in c:
-            if c_cmall not in d1:
+            if c_cmall not in self.d1:
                 a = a + c_cmall.title() + ' '
             else:
                 a = a + c_cmall + ' '
@@ -189,7 +193,7 @@ class stamps:
         b = self.message_text
         for i in range(len(self.checked)):
             if self.checked[i] is False:
-                if b[i][0] not in self.location and b[i][0] not in d1:
+                if b[i][0] not in self.location and b[i][0] not in self.d1:
                     self.baggage.append(b[i][0])
                     self.checked[i] = True
 
@@ -235,5 +239,8 @@ def ner_primitive(event):
     demo.just_location_plus()
     demo.uniquefy()
     #demo.baggage()
+    demo.view()
     ####################################################################
     return demo
+
+ner_primitive('set a reminder for tomorrow to get my car')
