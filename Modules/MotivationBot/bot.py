07567b737d56ea74259664b59d235818393dd7dc
Ruchir Patel <ruchir594@gmail.com>
2017-04-07 21:23:37 +0530

    adding lib features of AA

--- Modules/MotivationBot/bot.py
+++ Modules/MotivationBot/bot.py
@@ -1,6 +1,8 @@
 import sys
 sys.path.insert(0, '../../ActionsA')
 from nle.naive_entities import *
+from nlg.generate import *
 
 a = ner_primitive('How do you do?')
 a.view()
+generate('./conversation.csv')
