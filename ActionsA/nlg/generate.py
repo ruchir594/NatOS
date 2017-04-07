473d9cbeca14b9456147f6f2213f6e88178ad327
Ruchir Patel <ruchir594@gmail.com>
2017-04-07 21:46:20 +0530

    building pairs for simple NLG nltk module

--- ActionsA/nlg/generate.py
+++ ActionsA/nlg/generate.py
@@ -11,12 +11,18 @@ class NoFile(Exception):
     def __str__(self):
         return repr(self.arg)
 
+def build_pairs(f):
+    a=[]
+    for each in f:
+        a.append((each[0].replace('*', '(.*)').encode('string-escape'), tuple(each[1].split('^'))))
+    return tuple(a)
+
 def generate (path = 'undefined', sleepword = 'zebra'):
     if path == 'undefined':
         raise NoPath('A path is necessary')
     try:
         f = read_csv_by_row(path)
-        for each in f:
-            print each
+        pairs = build_pairs(f)
+        print pairs
     except Exception:
         raise NoFile('File not found at ' + path)
