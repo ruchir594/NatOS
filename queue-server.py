de4a0f241b17df14a298da246d7c3b08cf21d667
Nitesh Oswal <nit.oswal@gmail.com>
2017-04-20 17:04:53 +0530

    Queue Server, update requirements txt, remove celery from the stack

---
+++ queue-server.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+import sys
+from os import environ, path
+sys.path.append(path.join(path.dirname(path.abspath(__file__)), "ActionsA"))
+print sys.path
+from lib import tasktiger
+from redis import Redis
+import re
+from pkg_resources import load_entry_point
+
+environ.update({
+    "PYTHONPATH": "{0}{1}".format(environ["PYTHONPATH"] if "PYTHONPATH" in environ else "", ":" + path.dirname(path.join(path.abspath(__file__))))
+})
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    conn = Redis(db=1, decode_responses=True)
+    tiger = tasktiger.TaskTiger(connection=conn)
+    tiger.run_worker()
