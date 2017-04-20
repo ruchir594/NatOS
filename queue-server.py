b7bd9bdcddc10133ee4579d42ac14b5c26cf1341
Nitesh Oswal <nit.oswal@gmail.com>
2017-04-20 17:16:41 +0530

    queue server updates

--- queue-server.py
+++ queue-server.py
@@ -5,7 +5,6 @@ sys.path.append(path.join(path.dirname(path.abspath(__file__)), "ActionsA"))
 from lib import tasktiger
 from redis import Redis
 import re
-from pkg_resources import load_entry_point
 
 environ.update({
     "PYTHONPATH": "{0}{1}".format(environ["PYTHONPATH"] if "PYTHONPATH" in environ else "", ":" + path.dirname(path.join(path.abspath(__file__))))
@@ -13,6 +12,6 @@ environ.update({
 
 if __name__ == '__main__':
     sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
-    conn = Redis(db=1, decode_responses=True)
+    conn = Redis(db=0, decode_responses=True)
     tiger = tasktiger.TaskTiger(connection=conn)
     tiger.run_worker()
