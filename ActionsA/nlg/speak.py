ad01bcd86ca06f733e6bb67724189324bcf73b2c
ruchir594 <ruchir594@gmail.com>
2017-05-04 17:59:44 +0530

    speak change

--- ActionsA/nlg/speak.py
+++ ActionsA/nlg/speak.py
@@ -1,12 +1,15 @@
 # speak.py
 from gtts import gTTS
 import pygame
+import subprocess
 
 def say(ret):
     tts = gTTS(text=ret, lang='en')
     tts.save('ret.mp3')
-    pygame.mixer.init()
+    '''pygame.mixer.init()
     pygame.mixer.music.load('ret.mp3')
     pygame.mixer.music.play()
     while pygame.mixer.music.get_busy() == True:
-        continue
+        continue'''
+    listen = subprocess.Popen("omxplayer -o local ret.mp3", shell=True)
+    listen.wait()
