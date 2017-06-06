a27624bb615b9fb6f5e755646e98aab2e6ecd7e1
ruchir594 <ruchir594@gmail.com>
2017-06-06 21:23:52 +0530

    autobot

--- ActionsA/nlu/autobot.py
+++ ActionsA/nlu/autobot.py
@@ -3,6 +3,10 @@ class suger(object):
 
     # Everytime a user query comes in, this class will see if the standard CSV
     # FAQ style query can handle that response, if not, the bot will not respond
-    # and maybe a real user can check it out. 
+    # and maybe a real user can check it out.
+
     def __init__(self, incoming):
         self.qry=incoming
+
+    def spice(self, botclass):
+        
